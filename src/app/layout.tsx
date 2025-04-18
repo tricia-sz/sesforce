516eeba12ea13853b27aa47341d04db4332f3dc5
Patricia Souza <patricia.sz.br@gmail.com>
2025-04-17 23:40:42 -0300

    configurando autenticacao de usuarios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/shared/Header";
+import { AuthProvider } from "@/providers/auth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
+        <AuthProvider>
+           <Header />
+           {children}
+        </AuthProvider>
+         
       </body>
     </html>
   );
